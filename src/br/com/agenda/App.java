9692f88544459c17c72403ee1704ee317f377e1e
jmfcientista <jmfcientista@gmail.com>
2017-03-09 11:44:35 -0300

    first commit

---
+++ src/br/com/agenda/App.java
@@ -0,0 +1,10 @@
+package br.com.agenda;
+
+/**
+ * Created by mauricio on 08/03/17.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/com/classes/Feriados.java
@@ -0,0 +1,7 @@
+package br.com.classes;
+
+/**
+ * Created by mauricio on 08/03/17.
+ */
+public class Feriados {
+}
